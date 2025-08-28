819cd76d801b6bdb78a4ed8a7a38b774c6fe2a50
Workbob <teusalves@live.com>
2025-08-27 22:28:59 -0300

    Ajuste de Commit - Lista 5 (Correção de informação)

--- listas-e-exercicios/Lista_Exercicios_5/src/Lista5.java
+++ listas-e-exercicios/Lista_Exercicios_5/src/Lista5.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SalariosMinimos{
+public class Lista5{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int opcao;
