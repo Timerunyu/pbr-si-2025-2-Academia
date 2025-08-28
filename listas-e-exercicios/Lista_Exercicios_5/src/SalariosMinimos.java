d1b430b8d3ee761f03293efd0f5210705b6d76a5
Matheus-Alves <102559758+Workbob@users.noreply.github.com>
2025-08-27 21:32:40 -0300

    Atualizar o SalariosMinimos.java

--- listas-e-exercicios/Lista_Exercicios_5/src/SalariosMinimos.java
+++ listas-e-exercicios/Lista_Exercicios_5/src/SalariosMinimos.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Lista05 {
+public class SalariosMinimos{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int opcao;
